32584b323a0c08c500d7951b74a940d8951c9ee9
King Pea <zidanrizky1146@gmail.com>
2025-02-13 17:01:47 +0700

    init main pages and polish sidebar

--- resources/views/livewire/component/sidebar.blade.php
+++ resources/views/livewire/component/sidebar.blade.php
@@ -14,7 +14,7 @@
     </div>
 
     <ul>
-        <li class="flex items-center p-2 mb-2 bg-gray-700 rounded cursor-pointer">
+        <li class="flex items-center p-2 mb-2 rounded cursor-pointer hover:bg-gray-700">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="m-1 size-6">
                 <path fill-rule="evenodd"
                     d="M3 6a3 3 0 0 1 3-3h2.25a3 3 0 0 1 3 3v2.25a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3V6Zm9.75 0a3 3 0 0 1 3-3H18a3 3 0 0 1 3 3v2.25a3 3 0 0 1-3 3h-2.25a3 3 0 0 1-3-3V6ZM3 15.75a3 3 0 0 1 3-3h2.25a3 3 0 0 1 3 3V18a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3v-2.25Zm9.75 0a3 3 0 0 1 3-3H18a3 3 0 0 1 3 3V18a3 3 0 0 1-3 3h-2.25a3 3 0 0 1-3-3v-2.25Z"
